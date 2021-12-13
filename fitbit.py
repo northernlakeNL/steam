e6800dcc92a115a7d6e2579a6dff56bafbc4974d
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2021-12-13 21:01:22 +0100

    Update fitbit.py

--- fitbit.py
+++ fitbit.py
@@ -1,4 +1,4 @@
-import pip._vendor.requests
+import requests
 
 access_token= "eyJhdWQiOiIyM0JLRkwiLCJzdWIiOiI5NVM4RzIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIiLCJleHAiOjE2NDAwMjg3NDAsImlhdCI6MTYzOTQyNDA5N30"
 
