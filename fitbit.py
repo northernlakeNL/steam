75d6c67f8fdecb0f2c2ef2e48acaa27b7dc7afe0
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2021-12-14 00:42:45 +0100

    fitbit

--- fitbit.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-access_token= "eyJhdWQiOiIyM0JLRkwiLCJzdWIiOiI5NVM4RzIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIiLCJleHAiOjE2NDAwMjg3NDAsImlhdCI6MTYzOTQyNDA5N30"
-
-header = {'Authorization' : 'Bearer{}'.format(access_token)}
-response = requests.get("https://api.fitbit.com/1/user/-/profile.json", headers=header).json()
-
-print(response)
