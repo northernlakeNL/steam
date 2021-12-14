905315fd76c8f84e7d72353f2aa8a1d5329d3f7e
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2021-12-14 16:09:40 +0100

    .

---
+++ scherm.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import steam
+import json
+import os
+import statistics
+import pandas as pd
+import numpy
+import requests
+
+root = tk.Tk()
+
+root.title('Steam Add-on Project')
+root.state('iconic')
+root.geometry('720x480')
+
+numpy.array ([1,2,3,4])
+
+
+
+root.mainloop()
