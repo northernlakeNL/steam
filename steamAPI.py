db2dfef99d720c91ad713a208bbae8ff512cf738
Br3ndaNE <Brendan.endenburg@gmail.com>
2021-12-14 18:13:35 +0100

    Steam id Brendan toegevoegd
    Steam id Brendan toegevoegd

--- steamAPI.py
+++ steamAPI.py
@@ -1,8 +1,8 @@
 import requests
 
 API_key = "AF90EFF02499BB3CDDFFF28629DEA47B"
-steam_id = "76561198084867313"
-
-response = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={API_key}&steamids={steam_id}").json()
+steam_id_tom = "76561198084867313"
+steam_id_brendan = "76561198172219198"
+response = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={API_key}&steamids={steam_id_brendan}").json()
 
 print(response)
