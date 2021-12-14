841adc453e752265241d380dc95346bbde86fc2e
Br3ndaNE <Brendan.endenburg@gmail.com>
2021-12-14 22:22:48 +0100

    Update scherm2.py
    Shit gedaan qua zoeken in de json lijst. Het proberen te packen in tkinter scherm

--- scherm2.py
+++ scherm2.py
@@ -9,22 +9,62 @@ import requests
 S = open('steam.json')
 gamedata = json.load(S)
 
+
+
+
+
+
+
+
+
+
+
+
+#   Function place
+def clicked():   #Gegevens ophalen functie
+    game_naam = gamesbalk.get()
+#   Function place
+
+game_naam = ''
+
 root = Tk()
 root.title("Data")
-#   Function place
-def clicked(): #Gegevens ophalen functie
-    naam = gamesbalk.get()
-    test = Label(master=root, text=naam)
-    test.pack()
-#   Function place
 
-gamesbericht = Label(master=root, text="Voer hier een game in waar je de statestieken over wilt weten")
+gamesbericht = Label(master=root, text="Voer hier een game in waar je de statestieken over wilt weten") #text
 gamesbericht.pack()
 
-gamesbalk = Entry(master=root, width=50)
+gamesbalk = Entry(master=root, width=50)    #Textbalk
 gamesbalk.pack(padx=10, pady=10)
 
-gamesbutton = Button(master=root, text='Verzend gegevens', command=clicked)
+gamesbutton = Button(master=root, text='Verzend gegevens', command=clicked) #Button, gegevensophalen command. 
 gamesbutton.pack(pady=3)
+
+
+with open('steam.json') as Steam:
+    gamelist = json.load(Steam)
+
+
+    
+label1 = Label(master=root, text=name)
+label1.pack()
+label2 = Label(master=root, text=release_date)
+label2.pack()
+label3 = Label(master=root, text=price)
+label3.pack()
+label4 = Label(master=root, text=achievements)
+label4.pack()
+label5 = Label(master=root, text=developer)
+label5.pack()
+label6 = Label(master=root, text=platforms)
+label6.pack()
+
+for game in gamelist:
+    if game["name"] == game_naam:
+        name = game['name']
+        release_date = game['release_date']
+        price = game['price']
+        achievements = game['achievements']
+        developer = game['developer']
+        platforms = game['platforms']
 
 root.mainloop()
