4eb01ec068f6fef5d27b6d4fbd01d6232a3a7dff
Br3ndaNE <Brendan.endenburg@gmail.com>
2021-12-14 19:43:18 +0100

    Update steamAPI.py
    friendlistresponse

--- steamAPI.py
+++ steamAPI.py
@@ -3,6 +3,9 @@ import requests
 API_key = "AF90EFF02499BB3CDDFFF28629DEA47B"
 steam_id_tom = "76561198084867313"
 steam_id_brendan = "76561198172219198"
+
 response = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={API_key}&steamids={steam_id_brendan}").json()
 
-print(response)
+friendresponse = requests.get(f' http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={API_key}&steamid={steam_id_brendan}&relationship=friend').json()
+
+print(friendresponse)
