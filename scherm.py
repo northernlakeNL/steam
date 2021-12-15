68840e69071895711e3228c7dbab80945afe99bd
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2021-12-15 20:20:14 +0100

    Update scherm.py

--- scherm.py
+++ scherm.py
@@ -8,61 +8,34 @@ import pandas as pd
 import numpy
 import requests
 from urllib.request import urlopen
+from time import sleep
 
-#values
+
 API_key = 'AF90EFF02499BB3CDDFFF28629DEA47B'
 user_ID = '76561198172219198'
 
-#json bestand uitlezen
-with open('steam.json') as Steam:
-    gamelist = json.load(Steam)
 
-#game zoeken
-game_naam = input("Welke game zoek je: ")
-for game in gamelist:
-    if game["name"] == game_naam:
-        game_code = game['appid']
-        gamenaam = game['name']
-        releasedate = game['release_date']
-        prijs = game['price']
-        achievements = game['achievements']
-        developer = game['developer']
-        platforms = game['platforms']
+scherm = Tk()
+scherm.title('Steam Add-On project')
+scherm.geometry('720x480')
 
-URL = f'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={game_code}&key={API_key}&steamid={user_ID}'
+label1 = Label(scherm,
+                text= 'Game that you want')
+label1.pack()
 
-response = urlopen(URL)
-user_game_data = json.loads(response.read())
+def get_data():
+    test.config(text=""+text.get(1.0, "end-1c"))
 
-#vriendenlijst
-# friendlist = requests.get(URL).json()['friendlist']['friends']
-# steamidlist = []
-# for friend in range(len(friendlist)):
-#     steamidlist.append(friendlist[friend]['steamid'])
-# print(steamidlist)
+text = Text(scherm,width=80, height=15)
+text.insert(END, "")
+text.pack()
 
+knop = ttk.Button(scherm,
+                text='Submit',
+                command=get_data)
+knop.pack()
 
-#scherm
-root = Tk()
-root.title('Steam Add-on Project')
-root.state('iconic')
-root.geometry('720x480')
+test = Label(scherm, text="", font=18)
+test.pack()
 
-#widgets
-# name = Label(root, text=f'Naam:  {gamenaam}')
-# release = Label(root, text=f'Release Date:   {releasedate}')
-# developer = Label(root, text=f'Developer:   {developer}')
-# price = Label(root, text=f'Prijs:   {prijs}')
-# achievement = Label(root, text= f'Achievements: {achievements}')
-# platform = Label(root, text=f'Platforms:    {platforms}')
-gamedata = Label(root, text=f'User stats:   {user_game_data}')
-
-#plaatsing
-# name.grid(row=0, column=0, sticky='w')
-# release.grid(row=1, column=0, sticky='w')
-# developer.grid(row=2, column=0, sticky='w')
-# price.grid(row=3, column=0, sticky='w')
-# achievement.grid(row=4, column=0, sticky='w')
-# platform.grid(row=5, column=0, sticky='w')
-gamedata.grid(row=6, column=4,sticky='w')
-root.mainloop()
+scherm.mainloop()
