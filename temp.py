f1e080ab0861bd56ea810378b8bc628b1b9199b2
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2021-12-22 00:47:40 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,31 +1,82 @@
-#!/usr/bin/env python3
-
-# Import json module
+#alle imports die nodig zijn
+from tkinter import *
+from tkinter import ttk
 import json
+import os
+import statistics
+import pandas as pd
+import numpy
+import requests
+from urllib.request import urlopen
+from time import sleep
 
-# Define json data
-applicants ="""{
-  "Scott C Aldridge": "Present",
-  "Joe L Foss": "Present",
-  "Clyde M Gold": "Present",
-  "Monique C Doolittle": "Absent",
-  "David M Volkert": "Present",
-  "Israel M Oneal": "Present",
-  "Elizabeth M Groff": "Absent"
-}"""
-# Initialize a counter
-counter = 0
-# load the json data
-appList = json.loads(applicants)
-# iterate json to find the list of absent applicant
-for key in appList:
-    if (appList[key] == 'Absent'):
-  # Check the counter the print the message
-        if (counter == 0):
-            print("The following applicants are absent:")
-    print(key)
-    counter = counter + 1
+#values
+API_key = 'AF90EFF02499BB3CDDFFF28629DEA47B'
+user_ID = '76561198172219198'
 
-# Print the message if no applicant is absent
-if (counter == 0):
-  print("All applicants are present")
+#json bestand uitlezen
+with open('steam.json') as Steam:
+    gamelist = json.load(Steam)
+
+#game zoeken
+# game_naam = input("Welke game zoek je: ")
+# for game in gamelist:
+#     if game["name"] == game_naam:
+#         game_code = game['appid']
+#         gamenaam = game['name']
+#         releasedate = game['release_date']
+#         prijs = game['price']
+#         achievements = game['achievements']
+#         developer = game['developer']
+#         platforms = game['platforms']
+#URL compleet
+
+
+#Data halen uit de URL
+
+
+#scherm
+root = Tk()
+root.title('Steam Add-on Project')
+root.state('iconic')
+root.geometry('720x480')
+
+#widgets
+label1 = Label(root,
+                text='Game of which you want your data.',
+                font=18)
+label1.pack(pady=5)
+WantedData = Entry(root)
+WantedData.pack(side=BOTTOM)
+
+def gatherdata():
+    game = WantedData.get()
+    try:
+        for game in gamelist:
+            if game["name"] == game:
+                global gamenaam
+                gamenaam = game['name']
+                global game_code
+                game_code = game['appid']
+    except:
+        sleep(3.0)
+        exit()
+
+button=Button(root,
+                text='Search',
+                command=gatherdata)
+button.pack(pady=5)
+
+if button == True:
+    URL = f'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={game_code}&key={API_key}&steamid={user_ID}'
+    response = urlopen(URL)
+    user_game_data = json.loads(response.read())
+    gamedata = Label(root,
+                text=user_game_data,
+                font=18)
+    gamedata.pack(pady=5)
+    print(user_game_data)
+
+# gamedata = Label(root, text=f'User stats:   {user_game_data}')
+# gamedata.pack(side=LEFT)
+root.mainloop()
