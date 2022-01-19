ef7041a9f6bf2d96e0444833a243162b1ba79f2b
Greenjoepie <55410216+Greenjoepie@users.noreply.github.com>
2022-01-19 02:08:44 +0100

    Create sshpi.py
    code om matrix.py (LED voortgangsbalk) op de pi te runnen
    veel geluk dat te doen zonder m'n pi :)

---
+++ sshpi.py
@@ -0,0 +1,9 @@
+from fabric import Connection
+
+def ledbalk(voortgang):  # voortgang is % achievements
+    with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        PID = c.run('pgrep -f matrix.py', warn=True, hide=True).stdout.strip()
+        if PID:
+            c.run(f'kill {PID}', warn=True)
+    with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        c.run(f'python3 /home/pi/matrix.py -p {voortgang}')
