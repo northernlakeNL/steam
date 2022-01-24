e48a4efacf96a24f75df9bf1de9d5439ebc7b04f
Greenjoepie <55410216+Greenjoepie@users.noreply.github.com>
2022-01-24 16:58:26 +0100

    Update sshpi.py

--- sshpi.py
+++ sshpi.py
@@ -10,3 +10,15 @@ def ledbalk(voortgang):  # voortgang is % achievements
             c.run(f'python3 /home/pi/matrix.py -p {voortgang}')
     except TimeoutError:
         "lol"
+
+
+def vrienddisplay(naam):
+    try:
+        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+            PID = c.run('pgrep -f friend.py', warn=True, hide=True).stdout.strip()
+            if PID:
+                c.run(f'kill {PID}', warn=True)
+        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+            c.run(f'python3 /home/pi/hd44780/friend.py -n "{naam}"')
+    except TimeoutError:
+        "lol"
