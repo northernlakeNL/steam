76588845c3145001b71fb5effb17a0b805af22a1
Greenjoepie <55410216+Greenjoepie@users.noreply.github.com>
2022-01-24 16:10:46 +0100

    geen timeouterror als pi x.x is

--- sshpi.py
+++ sshpi.py
@@ -1,9 +1,12 @@
 from fabric import Connection
 
 def ledbalk(voortgang):  # voortgang is % achievements
-    with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
-        PID = c.run('pgrep -f matrix.py', warn=True, hide=True).stdout.strip()
-        if PID:
-            c.run(f'kill {PID}', warn=True)
-    with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
-        c.run(f'python3 /home/pi/matrix.py -p {voortgang}')
+    try:
+        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+            PID = c.run('pgrep -f matrix.py', warn=True, hide=True).stdout.strip()
+            if PID:
+                c.run(f'kill {PID}', warn=True)
+        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+            c.run(f'python3 /home/pi/matrix.py -p {voortgang}')
+    except TimeoutError:
+        "lol"
