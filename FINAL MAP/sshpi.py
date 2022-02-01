c4fa6e1484b31e9176b6fd9fe84b35aa13d0db79
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2022-02-01 16:54:01 +0100

    school work

--- FINAL MAP/sshpi.py
+++ FINAL MAP/sshpi.py
@@ -3,11 +3,11 @@ from fabric import Connection
 
 def ledbalk(voortgang):  # voortgang is % achievements
     try:
-        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        with Connection('dnsmies.ooguy.com', user='pi', connect_kwargs={"password": "Roots"}) as c:
             PID = c.run('pgrep -f matrix.py', warn=True, hide=True).stdout.strip()
             if PID:
                 c.run(f'kill {PID}', warn=True)
-        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        with Connection('dnsmies.ooguy.com', user='pi', connect_kwargs={"password": "Roots"}) as c:
             c.run(f'python3 /home/pi/matrix.py -p {voortgang}')
     except TimeoutError:
         "lol"
@@ -15,11 +15,11 @@ def ledbalk(voortgang):  # voortgang is % achievements
 
 def vrienddisplay(naam):  # naam is naam :)
     try:
-        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        with Connection('dnsmies.ooguy.com', user='pi', connect_kwargs={"password": "Roots"}) as c:
             PID = c.run('pgrep -f friend.py', warn=True, hide=True).stdout.strip()
             if PID:
                 c.run(f'kill {PID}', warn=True)
-        with Connection('swaggerlord.ooguy.com', user='pi', connect_kwargs={"password": "coolcool"}) as c:
+        with Connection('dnsmies.ooguy.com', user='pi', connect_kwargs={"password": "Roots"}) as c:
             c.run(f'python3 /home/pi/hd44780/wave.py')
             c.run(f'python3 /home/pi/hd44780/friend.py -n "{naam}"')
     except TimeoutError:
